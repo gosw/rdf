581b302171ad03e2414452f499a07c633a756305
gosw_cp <gosw@users.noreply.github.com>
2017-02-02 16:30:00 +0000

    NEW - sheego.Framework: created solution, build base folder structure

---
+++ sheego.Framework/Domain/DomainTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sheego.Framework.Domain.Tests")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("070d9b62-5080-4b9c-8a0b-9ad5ac556e22")]
