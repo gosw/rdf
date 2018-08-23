7c174c44bfed7f21d7bc195b35696facf8decff4
gosw <gosw@users.noreply.github.com>
2018-08-23 16:56:06 +0200

    CHG - sheego.Framework: ReSharper refactoring - adding namespace in Web project

--- sheego.Framework/Presentation/Web/App_Start/FilterConfig.cs
+++ sheego.Framework/Presentation/Web/App_Start/FilterConfig.cs
@@ -1,7 +1,6 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace Web
+namespace sheego.Framework.Presentation.Web
 {
     public class FilterConfig
     {
--- sheego.Framework/Presentation/Web/Authorization/FrameworkAuthorizationAttribute.cs
+++ sheego.Framework/Presentation/Web/Authorization/FrameworkAuthorizationAttribute.cs
@@ -1,9 +1,4 @@
-﻿using sheego.Framework.Domain.Shared;
-using sheego.Framework.Domain.Shared.Locator;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using sheego.Framework.Domain.Shared.Locator;
 using System.Web.Mvc;
 using System.Web.Routing;
 
